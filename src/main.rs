7d908b1e25e39c08754875c11627ee9bf66190ee
htoopyaelinn56 <hplinn0@gmail.com>
2025-11-13 20:17:49 +0630

    refactor: move LAN discovery logic to multicast_service module

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ async fn main() {
     let args: Vec<String> = std::env::args().collect();
     let player_name = args.get(1).cloned().unwrap_or_else(|| "Player".into());
 
-    rust_udp_multicast_test::start_service(player_name).await;
+    rust_udp_multicast_test::multicast_service::start_service(player_name).await;
 }
